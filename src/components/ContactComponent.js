126d40f205f9539e6add6d0886b17b186e7887f7
Raushan <raushankumar606@gmail.com>
2020-08-09 13:51:27 +0530

    contact icons

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,8 +1,13 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 export default function ContactComponent() {
   return (
-    <div>
+    <div className="mb-4">
       <div className="row mt-4">
         <div className="col-4">
           <hr className="hrr"></hr>
@@ -14,9 +19,17 @@ export default function ContactComponent() {
           <hr className="hrr"></hr>
         </div>
       </div>
-      <div className="row">
-        <div className="col-12">
-          <i class="fab fa-github"></i>
+      <div className="row ">
+        <div className="col-12 d-flex justify-content-center">
+          <a href="https://github.com/raushan606" target="_blank">
+            <FontAwesomeIcon color="black" className="mx-2" icon={faGithub} />
+          </a>
+          <a href="https://twitter.com/_var_const" target="_blank">
+            <FontAwesomeIcon color="black" className="mx-2" icon={faTwitter} />
+          </a>
+          <a href="https://www.linkedin.com/in/raushan606/" target="_blank">
+            <FontAwesomeIcon color="black" className="mx-2" icon={faLinkedin} />{" "}
+          </a>
         </div>
       </div>
     </div>
